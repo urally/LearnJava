75331a5ca3de7a4aeb40b0d0dfd0160a0639ede5
urally <urally@yeah.net>
2020-08-06 19:13:41 +0800

    updata:
    .

--- src/com/urally/pvt/Aug05/LearnCollection.java
+++ src/com/urally/pvt/Aug05/LearnCollection.java
@@ -1,4 +1,14 @@
 package com.urally.pvt.Aug05;
-
+import java.util.ArrayList;
+import java.util..Collection;
+//Collection是所有单列集合的父接口,因此在Collection中定义了单列集合(List和Set)通用的一些方法
+//这些方法可用于操作所有的单列集合.
+// public boolean add(E e);
+// public void clear();
+// public boolean contains(E e);
+// public boolean isEmpty();
+// public int size();
+// public Object[] toArray)_
 public class LearnCollection {
+
 }
